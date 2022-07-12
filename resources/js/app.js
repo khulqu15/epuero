5d0a0260cc0abaa1bbc34029f39af923b6c07bd9
Imam Shofiuddin <imamshofiuddin00@gmail.com>
2022-07-12 20:41:39 +0700

    setup firebase

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import './bootstrap';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import { initializeApp } from "firebase/app";
+import { getDatabase , ref, set } from "firebase/database";
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -18,3 +20,34 @@ createInertiaApp({
 });
 
 InertiaProgress.init({ color: '#4B5563' });
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAC-rabyrVT2fBAN6MvzOhhpBwcnpx48iU",
+  authDomain: "epuero-5f880.firebaseapp.com",
+  projectId: "epuero-5f880",
+  storageBucket: "epuero-5f880.appspot.com",
+  messagingSenderId: "267893736576",
+  appId: "1:267893736576:web:7ebd6da2fb4d430b455265",
+  databaseURL: "https://epuero-5f880-default-rtdb.firebaseio.com"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Connect db
+const db = getDatabase();
+
+// Coba insert data
+writeLocationData('1','123683762836','93793598579');
+writeLocationData('2','3762836','93793598579');
+writeLocationData('3','123683766','9379');
+
+function writeLocationData(LocId, Latitude, Langitude) {
+  set(ref(db, 'location/' + LocId), {
+    id_location: LocId,
+    latitude: Latitude,
+    langitude: Langitude,
+  });
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,6 +27,7 @@
         @inertiaHead
         <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
         <script src="https://cdn.tailwindcss.com"></script>
+
     </head>
     <body class="font-montserrat antialiased">
         @inertia
